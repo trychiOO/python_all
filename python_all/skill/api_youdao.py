f0c90fcdd04a3a98d46653c57a759ec10025ab49
trychiOO <1162551619@qq.com>
2020-03-10 14:47:28 +0800

    youdao

---
+++ python_all/skill/api_youdao.py
@@ -0,0 +1,66 @@
+#coding:utf-8
+import sys
+import uuid
+import requests
+import hashlib
+import time
+from imp import reload
+
+import time
+
+reload(sys)
+
+YOUDAO_URL = 'https://openapi.youdao.com/api'
+APP_KEY = '1ff92dd6c3113fe1'
+APP_SECRET = 'y4cUK8oNOBkJW3rpyhuPKB7J1zauJ95F'
+
+
+def encrypt(signStr):
+    hash_algorithm = hashlib.sha256()
+    hash_algorithm.update(signStr.encode('utf-8'))
+    return hash_algorithm.hexdigest()
+
+
+def truncate(q):
+    if q is None:
+        return None
+    size = len(q)
+    return q if size <= 20 else q[0:10] + str(size) + q[size - 10:size]
+
+
+def do_request(data):
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    return requests.post(YOUDAO_URL, data=data, headers=headers)
+
+
+def connect():
+    q = "绿色"
+
+    data = {}
+    data['from'] = '源语言'
+    data['to'] = '目标语言'
+    data['signType'] = 'v3'
+    curtime = str(int(time.time()))
+    data['curtime'] = curtime
+    salt = str(uuid.uuid1())
+    signStr = APP_KEY + truncate(q) + salt + curtime + APP_SECRET
+    sign = encrypt(signStr)
+    data['appKey'] = APP_KEY
+    data['q'] = q
+    data['salt'] = salt
+    data['sign'] = sign
+
+    response = do_request(data)
+    contentType = response.headers['Content-Type']
+    if contentType == "audio/mp3":
+        millis = int(round(time.time() * 1000))
+        filePath = "D:\PycharmProjects\python_all\skill\au" + str(millis) + ".mp3"
+        fo = open(filePath, 'wb')
+        fo.write(response.content)
+        fo.close()
+    else:
+        print(response.content)
+
+
+if __name__ == '__main__':
+    connect()
